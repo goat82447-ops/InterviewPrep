3448124713254248c26d7788d4d41fdbaef86ea8
goat82447-ops <goat82447@gmail.com>
2026-07-31 15:14:55 +0530

    Add Settings link to AgentPage

--- Web/AgentPage.cs
+++ Web/AgentPage.cs
@@ -48,6 +48,7 @@ internal static class AskPage
         sb.Append("<a class=\"chip\" href=\"/live\">\ud83d\udcf9 Live interview</a>");
         sb.Append("<a class=\"chip\" href=\"/drills\">\u26a1 Rapid drills</a>");
         sb.Append("<a class=\"chip\" href=\"/plan\">\ud83d\uddd3\ufe0f Study plan</a>");
+        sb.Append("<a class=\"chip\" href=\"/settings\">\u2699\ufe0f Settings</a>");
         sb.Append("</div>");
 
         sb.Append("<form method=\"post\" action=\"/ask\">");
