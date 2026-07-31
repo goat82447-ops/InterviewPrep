4dc28a629a58b9c3e177712ed0eb1bfcc1135a78
goat82447-ops <goat82447@gmail.com>
2026-07-31 15:18:43 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- Web/PracticePage.cs
+++ Web/PracticePage.cs
@@ -47,6 +47,7 @@ internal static class PracticePage
         sb.Append("<a class=\"chip\" href=\"/live\">\ud83d\udcf9 Live interview</a>");
         sb.Append("<a class=\"chip\" href=\"/drills\">\u26a1 Rapid drills</a>");
         sb.Append("<a class=\"chip\" href=\"/plan\">\ud83d\uddd3\ufe0f Study plan</a>");
+        sb.Append("<a class=\"chip\" href=\"/settings\">\u2699\ufe0f Settings</a>");
         sb.Append("</div>");
 
         // Topic chips
