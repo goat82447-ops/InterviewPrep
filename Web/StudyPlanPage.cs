e49ee08acc921dd789da464384d4978622b94b87
goat82447-ops <goat82447@gmail.com>
2026-07-31 15:20:04 +0530

    Update StudyPlanPage.cs

--- Web/StudyPlanPage.cs
+++ Web/StudyPlanPage.cs
@@ -46,6 +46,7 @@ internal static class StudyPlanPage
         sb.Append("<a class=\"chip\" href=\"/live\">\ud83d\udcf9 Live interview</a>");
         sb.Append("<a class=\"chip\" href=\"/drills\">\u26a1 Rapid drills</a>");
         sb.Append("<a class=\"chip active\" href=\"/plan\">\ud83d\uddd3\ufe0f Study plan</a>");
+        sb.Append("<a class=\"chip\" href=\"/settings\">\u2699\ufe0f Settings</a>");
         sb.Append("</div>");
 
         sb.Append("<div class=\"intro\">Each day takes about 45\u201360 minutes. Do the four steps in order: " +
